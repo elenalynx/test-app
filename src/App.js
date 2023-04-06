902ed2a2ed1fb76baaf18d947a90d21e68bfbc47
elenalynx <elnlynx@gmail.com>
2023-04-07 00:04:29 +0300

    state(first vers)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-// import {Component, StrictMode} from "react";
+import {Component, StrictMode} from "react";
 import './App.css';
 
 // function WhoAmI (props) {
-function WhoAmI ({name, surname, link}) {
-    return (
-        <div>
-            <h1>My name is {name()}, surname - {surname}</h1>
-            <a href={link}>My profile</a>
-        </div>
-    )
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 25,
+            text: '+++'
+        }
+    }
+
+    nextYear = () => {
+        // console.log('+++')
+      this.setState(state => ({
+          years: state.years + 1
+      }))
+    }
+
+    render() {
+        const {name, surname, link} = this.props;
+        return (
+            <div>
+                <button onClick={this.nextYear}>{this.state.text}</button>
+                <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+                <a href={link}>My profile</a>
+            </div>
+        )
+    }
+
 }
 
 function App() {
     return (
         <div className="App">
             <StrictMode>
-                {/*<WhoAmI name="John" surname="Smith" link="google.com"/>*/}
-                <WhoAmI name={() => {return "John"}} surname="Smith" link="google.com"/>
-                <WhoAmI name={() => {return "Lili"}} surname="Mili" link="google.com"/>
+                <WhoAmI name='John' surname="Smith" link="google.com"/>
+                <WhoAmI name='Lili' surname="Smith2" link="google.com"/>
             </StrictMode>
 
         </div>
--- src/index.js
+++ src/index.js
@@ -2,26 +2,9 @@ import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import {Header} from "./App";
-
-// const elem = <h2>Hello!</h2>
-// const elem = React.createElement('h2', {className: 'qwerty'}, 'Hello')
-
-
-// const text = 'Hello!!'
-
-// const elem = (
-//     <div>
-//         <h2 className="text">Text: {text}</h2>
-//         <input type="text" />
-//         <label htmlFor=""></label>
-//         <button tabIndex="0">Click</button>
-//     </div>
-// )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // elem
   <StrictMode>
     <App />
   </StrictMode>
