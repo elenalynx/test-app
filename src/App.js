d95f7652330d54c4a26bbd6c4317ce5da8f32d58
elenalynx <elnlynx@gmail.com>
2023-04-13 18:03:05 +0300

    add info, lsn 132

--- src/App.js
+++ src/App.js
@@ -7,9 +7,19 @@ class WhoAmI extends Component {
         super(props);
         this.state = {
             years: 25,
-            text: '+++'
+            text: '+++',
+            position: ''
         }
+        // this.nextYear = this.nextYear.bind(this)
     }
+
+    // 2, 3 variant
+    // nextYear() {
+    //     // console.log('+++')
+    //   this.setState(state => ({
+    //       years: state.years + 1
+    //   }))
+    // }
 
     nextYear = () => {
         // console.log('+++')
@@ -18,13 +28,30 @@ class WhoAmI extends Component {
       }))
     }
 
+    commitInputChanges = (e, color) => {
+        // console.log(e.target.value)
+        console.log(color)
+        this.setState({
+            position: e.target.value
+        })
+    }
+
     render() {
         const {name, surname, link} = this.props;
+        const {position, years} = this.state;
         return (
             <div>
+                {/*<button onClick={() => this.nextYear()}>{this.state.text}</button>*/}
                 <button onClick={this.nextYear}>{this.state.text}</button>
-                <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+                <h1>My name is {name}, surname - {surname},
+                    age - {years},
+                    position - {position}</h1>
                 <a href={link}>My profile</a>
+                <form>
+                    <span>Введите должность</span>
+                    {/*<input type="text" onChange={this.commitInputChanges} />*/}
+                    <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+                </form>
             </div>
         )
     }
